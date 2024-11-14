26e681c69e56b2bc4edbeba9ea9565379cb37010
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-11-14 17:10:32 +0200

    react_keyboard by Dimoshua

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type Props = {
+  message: string;
+};
+
+export class App extends React.Component<Props> {
+  state: Props = {
+    message: 'Nothing was pressed yet',
+  };
+
+  handleKeyUp = (e: KeyboardEvent) => {
+    this.setState({ message: `The last pressed key is [${e.key}]` });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render(): React.ReactNode {
+    const { message } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
