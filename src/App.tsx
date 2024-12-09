c71d4d0fdbed199a54a32a1a080777537d815a71
Maksym Seliutin <maxwell7k@gmail.com>
2024-12-09 19:48:13 +0200

    Fix: deleted console.log()

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
 import React from 'react';
 
-
-
 export class App extends React.Component {
   state = {
     pressedKey: '',
-  }
+  };
 
   handleKeyUp = (event: KeyboardEvent) => {
-    this.setState({pressedKey:event.key})
-      console.log(event.key);
-  }
+    this.setState({ pressedKey: event.key });
+  };
 
   componentDidMount(): void {
-    document.addEventListener('keyup', this.handleKeyUp)
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
 
-  }
   componentWillUnmount(): void {
-      document.removeEventListener('keyup', this.handleKeyUp)
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
+
   render() {
     return (
       <div className="App">
         <p className="App__message">
-          {this.state.pressedKey ?
-            `The last pressed key is [${this.state.pressedKey}]`
-            : `Nothing was pressed yet`}</p>
+          {this.state.pressedKey
+            ? `The last pressed key is [${this.state.pressedKey}]`
+            : `Nothing was pressed yet`}
+        </p>
       </div>
-    )
+    );
   }
- }
+}
 
 // The last pressed key is {[Enter]}
 
