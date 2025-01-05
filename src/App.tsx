5164f4655446bc597448d11a3f5f20acf512c84f
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-01-05 13:22:17 +0100

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  key: string;
+};
+
+export class App extends React.Component {
+  state: Readonly<State> = {
+    key: 'Nothing was pressed yet',
+  };
+
+  handleKey = () => {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ key: event.key });
+    });
+  };
+
+  componentDidMount() {
+    this.handleKey()
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <p className="App__message">The last pressed key is {this.state.key}</p>
+      </div>
+    );
+  }
+}
