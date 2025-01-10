2d092656255869c8bb5c875e236c31a433f8ec48
nex1994 <bilicki1994@gmail.com>
2025-01-10 14:27:05 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string | null;
+};
+
+export class App extends React.Component<{}, State> {
+  state = {
+    pressedKey: null,
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', (event: KeyboardEvent) => {
+      this.setState({ pressedKey: event.key });
+    });
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', () => {
+      this.setState({ pressedKey: null });
+    });
+  }
+
+  render() {
+    if (this.state.pressedKey === null) {
+      return <p className="App__message">Nothing was pressed yet</p>;
+    } else if (this.state.pressedKey !== null) {
+      return (
+        <p className="App__message">
+          The last pressed key is [{this.state.pressedKey}]
+        </p>
+      );
+    }
+  }
+}
