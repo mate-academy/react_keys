2f113bfb0992e8af194bc6dd360bf520f86b01bd
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-01-14 11:06:51 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  prevKey: string;
+};
+
+export class App extends React.Component<{}, State> {
+  state: State = {
+    prevKey: '',
+  };
+
+  handleGlobalClick = (event: KeyboardEvent) => {
+    this.setState({ prevKey: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleGlobalClick);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleGlobalClick);
+  }
+
+  render() {
+    const { prevKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {!prevKey
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${this.state.prevKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
+
+// export const App: React.FC = () => (
+//   <div className="App">
+//     <p className="App__message">The last pressed key is [Enter]</p>
+//   </div>
+// );
