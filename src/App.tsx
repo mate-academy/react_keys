7b9f4f55d2d5d2bc18debd35915e155c755f754d
Denys Raboshchuk <kontroler444@gmail.com>
2025-01-21 11:31:40 +0200

    Create React keyboard key handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+// export const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <p className="App__message">Nothing was pressed yet</p>
+//     </div>
+//   );
+// };
+
+type State = {
+  keyboardKey: string;
+};
+
+export class App extends React.Component<State> {
+  state: State = {
+    keyboardKey: '',
+  };
+
+  handleKeyChange = (event: KeyboardEvent) => {
+    this.setState({
+      keyboardKey: event.key,
+    });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyChange);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyChange);
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <p className="App__message">
+          {this.state.keyboardKey
+            ? `The last pressed key is [${this.state.keyboardKey}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
